77b71fe97432817d7c0fa50e97a082db1ec3b5c2
Joshua Lokken <joshualokken@pm.me>
2022-08-18 20:35:42 -0500

    Update db connection log string

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -14,7 +14,7 @@ const start = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to the database...");
+    console.log("Connected to the database.");
   } catch (error) {
     console.error(error);
   }
